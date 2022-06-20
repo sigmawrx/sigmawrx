e89e2c39792323d14e208b6e4578c8d7a9dcb595
sigmawrx <107821600+sigmawrx@users.noreply.github.com>
2022-06-19 21:33:22 -0400

    Add files via upload
    here you go

---
+++ FISHCAKES.py
@@ -0,0 +1,10 @@
+
+for num in range (1,100):       
+    if num % 15 is 0:
+        print("FISHCAKES")
+    elif num % 3 is 0:
+        print("FISH")
+    elif num % 5 is 0:
+        print("CAKES")
+    else:
+        print (num)
